10049208571947f6422bdfc293a7accc30f50ebf
Cassandra Collins <103472275+CassandraRose12@users.noreply.github.com>
2023-02-24 14:27:04 -0500

    testing a application

---
+++ src/LogIn.js
@@ -0,0 +1,14 @@
+import './LogIn.scss';
+import React from 'react';
+import dotenv from 'dotenv';
+
+// call the .env file
+dotenv.config();
+
+//allows the component to access all the properties and functions
+class LogIn extends React.Component {
+    handleSubmit = async (event) => {
+        
+    }
+
+}
